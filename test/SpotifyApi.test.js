081af5990284fb51ae75521e7b969ad38fde0001
Jesus Paz <jeherpabo@gmail.com>
2019-09-12 22:14:22 -0500

    Add the test to Spotify Api, using search to find a number of tracks

--- test/SpotifyApi.test.js
+++ test/SpotifyApi.test.js
@@ -2,7 +2,6 @@ const request = require('request');
 const { expect, assert } = require('chai');
 const statusCode = require('http-status-codes');
 
-
 describe('Spotify Api Test', () => {
   it('Test the Spotify access token', () => {
     const requestBody = {
@@ -20,6 +19,37 @@ describe('Spotify Api Test', () => {
       assert.isNull(error);
       expect(response.statusCode).to.equal(statusCode.OK);
       assert.exists(body.access_token, 'The token do not exists');
+    });
+  });
+
+  it('Test the search of the song "Till I Collapse" on Spotify', () => {
+    const requestBodyAuth = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        grant_type: 'client_credentials'
+      },
+      headers: {
+        Authorization: `Basic ${Buffer.from(`${process.env.clientId}:${process.env.clientSecret}`).toString('base64')}`
+      },
+      json: true
+    };
+
+    request.post(requestBodyAuth, (errorAuth, responseAuth, bodyAuth) => {
+      const requestBodySearch = {
+        url: 'https://api.spotify.com/v1/search?q=Till%20I%20Collapse&type=track',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${bodyAuth.access_token}`
+        },
+        json: true
+      };
+
+      request.get(requestBodySearch, (errorSearch, responseSearch, bodySearch) => {
+        assert.isNull(errorSearch);
+        expect(responseSearch.statusCode).to.equal(statusCode.OK);
+        expect(bodySearch.tracks.items).to.have.lengthOf(20);
+      });
     });
   });
 });
